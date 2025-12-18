98b6e7879ded951e51bd10d01d3b505a26a17285
tahsinTH007 <tahsin.hassan007@gmail.com>
2025-12-18 11:47:52 +0600

    $npm_config_msg

---
+++ src/models/user.model.ts
@@ -0,0 +1,62 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercare: true,
+      trim: true,
+    },
+
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+    },
+
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    name: {
+      type: String,
+    },
+
+    twoFactorEnabled: {
+      type: Boolean,
+      default: false,
+    },
+
+    twoFactorSecret: {
+      type: String,
+      default: undefined,
+    },
+
+    tokenVersion: {
+      type: Number,
+      default: 0,
+    },
+
+    resetPasswordToken: {
+      type: String,
+      default: undefined,
+    },
+
+    resetPasswordExpires: {
+      type: Date,
+      default: undefined,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = model("User", userSchema);
